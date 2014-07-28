8bab7bd88a5bbfda31129c376d99e5f37b75ef42
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-07-28 23:35:16 +0530

    axxion specific changes
    axxion specific changes
    
    fix
    
    fix

--- axxion.mk
+++ axxion.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/axxion/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GalaxyGrandDuos
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/axxion/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9082/device_i9082.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9082
-PRODUCT_NAME := cm_i9082
+PRODUCT_NAME := axxion_i9082
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9082

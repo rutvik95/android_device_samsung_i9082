00674602ae1126c6235a1ce1f3a885d1a2a1b76b
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-08-03 12:10:24 +0530

    fix error while lunching the device
    remove gps_us_supl.mk

--- device_i9082.mk
+++ device_i9082.mk
@@ -1,7 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
 
 $(call inherit-product-if-exists, vendor/samsung/i9082/i9082-vendor.mk)
 
